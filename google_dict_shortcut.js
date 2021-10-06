678800dc1858f5f6abab58d423bb1ca211853906
David Pu <pulq.ustc@gmail.com>
2021-10-06 11:50:21 -0700

    Update google_dict_shortcut.js

--- google_dict_shortcut.js
+++ google_dict_shortcut.js
@@ -21,34 +21,76 @@
         return el;
     }
 
-    // shortcuts for Google dictionary
-    document.addEventListener('keydown', function(event) {
-     if (!event.ctrlKey) {
-         return;
-     }
-     if (event.ctrlKey && (event.key === 'a' || event.key === 'o')) {
-       event.preventDefault();
-       document.getElementsByTagName('audio')[0].click();
-     } else if (event.ctrlKey && (event.key === 's' || event.key === 'l')) {
-        event.preventDefault();
-
+    function save2list(remove) {
         var val = search_el().value;
         var store = window.localStorage;
         store.setItem('l:' + val, val);
         var el = document.querySelector("span[data-dobid='hdw']");
         var txt = el.innerText;
         if (el && txt[txt.length - 1] === '❤') {
-            el.innerText = txt.slice(0, txt.length - 1);
+            if (remove) {
+                el.innerText = txt.slice(0, txt.length - 1);
+            }
         } else {
             el.innerText += '❤';
         }
-     } else if (event.ctrlKey && (event.key === 'f' || event.key === 'p')) {
-        event.preventDefault();
-        var inputs = document.getElementsByTagName('input');
-        var el2 = search_el();
-        el2.focus();
-        el2.select();
-     }
-    });
+    }
 
+    function abs_offset(element) {
+        var top = 0, left = 0;
+        do {
+            top += element.offsetTop || 0;
+            left += element.offsetLeft || 0;
+            element = element.offsetParent;
+        } while(element);
+
+        return {
+            top: top,
+            left: left
+        };
+    }
+
+    function pretty_page() {
+        var el = search_el();
+        if (!el) {
+            return;
+        }
+        var absoff = abs_offset(search_el());
+        if (absoff.top > 0) {
+            window.scroll(0, absoff.top);
+        }
+
+        el = document.getElementById('searchform');
+        if (el) {
+            el.remove();
+        }
+    }
+
+    // shortcuts for Google dictionary
+    document.addEventListener('keydown', function(event) {
+        if (!event.ctrlKey && !event.altKey) {
+            return;
+        }
+        if ((event.key === 'a' || event.key === 'o')) {
+            event.preventDefault();
+            document.getElementsByTagName('audio')[0].click();
+            save2list(false);
+        } else if ((event.key === 's' || event.key === 'l')) {
+            event.preventDefault();
+            save2list(true);
+        } else if ((event.key === 'f' || event.key === 'p')) {
+            event.preventDefault();
+            var inputs = document.getElementsByTagName('input');
+            var el2 = search_el();
+            el2.focus();
+            el2.select();
+            pretty_page();
+        }
+    });
+    setTimeout(pretty_page, 500);
+    document.addEventListener('keyup', function(event) {
+        if (event.key === 'Enter') {
+            setTimeout(pretty_page, 200);
+        }
+    });
 })();
